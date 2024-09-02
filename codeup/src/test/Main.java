ef4398fd278d1feef87b316896c466791c791228
johe01 <jhe0598@gmail.com>
2024-09-02 18:24:09 +0900

    걍 수정

--- codeup/src/test/Main.java
+++ codeup/src/test/Main.java
@@ -7,24 +7,13 @@ public class Main {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-		int a = sc.nextInt();
-	
-		switch (a) {
-			case 12:
-			case 1:
-			case 2: System.out.println("winter"); break;
-			case 3:
-			case 4:
-			case 5: System.out.println("spring"); break;
-			case 6:
-			case 7:
-			case 8: System.out.println("summer"); break;
-			case 9:
-			case 10:
-			case 11: System.out.println("fall"); break;
+		String a = sc.nextLine();
+		String[] str = a.split(" ");
+		for (int i = 0; i < str.length; i++) {
+			if (Integer.parseInt(str[i])==0) break;
+			System.out.println(str[i]);
 		}
 		
-		sc.close();
 		
 	}
 }
